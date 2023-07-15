d9b93a06522cdff48d19a339390c0673f80b7e6e
James Cuzella <james.cuzella@lyraphase.com>
2023-07-14 19:19:36 -0600

    Add GPLv3 license

--- preview-foot-themes.sh
+++ preview-foot-themes.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+#    preview-foot-themes.sh - Easily preview foot terminal themes via fzf + exa + neofetch
+#
+#    Copyright (C) 2023  James Cuzella
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 theme_dir=${1:-/usr/share/foot/themes/}
 
