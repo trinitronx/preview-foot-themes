7317416f2d6012f78866e1b396e6c081dd0f7cec
James Cuzella <james.cuzella@lyraphase.com>
2023-07-23 03:27:27 -0600

    preview-terminal-colors.sh: Fix bash-isms: SC3028, SC3054
    Warnings were:
    
        In preview-terminal-colors.sh line 2:
        SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
                                       ^---------------^ SC3028 (warning): In POSIX sh, BASH_SOURCE is undefined.
                                       ^---------------^ SC3054 (warning): In POSIX sh, array references are undefined.

--- preview-terminal-colors.sh
+++ preview-terminal-colors.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/zsh
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/bin/sh
+SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd )"
 
 exa --icons --color-scale --classify -lah --tree $SCRIPT_DIR/foot-theme-demo/
 neofetch
