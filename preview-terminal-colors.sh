382c17063418452dc1ef6dc100049f14d789b947
James Cuzella <james.cuzella@lyraphase.com>
2023-07-23 03:28:54 -0600

    preview-terminal-colors.sh: Fix SC2086
    Warnings were:
    
        In preview-terminal-colors.sh line 4:
        exa --icons --color-scale --classify -lah --tree $SCRIPT_DIR/foot-theme-demo/
                                                         ^---------^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
        Did you mean:
        exa --icons --color-scale --classify -lah --tree "$SCRIPT_DIR"/foot-theme-demo/
    
        In preview-terminal-colors.sh line 6:
        sleep ${1:-3}
              ^-----^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
        Did you mean:
        sleep "${1:-3}"

--- preview-terminal-colors.sh
+++ preview-terminal-colors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd )"
 
-exa --icons --color-scale --classify -lah --tree $SCRIPT_DIR/foot-theme-demo/
+exa --icons --color-scale --classify -lah --tree "$SCRIPT_DIR/foot-theme-demo/"
 neofetch
-sleep ${1:-3}
+sleep "${1:-3}"
